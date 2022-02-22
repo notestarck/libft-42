724d301091624101958dc8ec44d1f0d95e58a989
estarck <estarck@me.com>
2022-02-22 21:10:35 +0100

    strrchr

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: estarck <estarck@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 08:37:47 by estarck           #+#    #+#             */
-/*   Updated: 2022/02/22 20:05:45 by estarck          ###   ########.fr       */
+/*   Updated: 2022/02/22 21:09:15 by estarck          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+char 	*ft_strrchr(const char *s, int c);
 
 int main(void)
 {
@@ -110,9 +111,16 @@ int main(void)
 	return 0;
 	*/
 
+	/*
 	char c = 'E';
 	printf("%d", ft_tolower(c));
 
 	return 0;
+	*/
 
+	char str[] = "Bonjour !";
+	char c = '!';
+	printf("%s", ft_strrchr(str, c));
+
+	return (0);
 }
