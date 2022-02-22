3ea12ae681c8bd87106483c6948cc360467e5804
estarck <estarck@me.com>
2022-02-22 19:03:27 +0100

    strl...

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,13 +6,13 @@
 /*   By: estarck <estarck@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 18:23:36 by estarck           #+#    #+#             */
-/*   Updated: 2022/02/22 18:34:53 by estarck          ###   ########.fr       */
+/*   Updated: 2022/02/22 18:36:24 by estarck          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-size_t	ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize)
+size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
 {
 	unsigned int	i;
 	unsigned int	c;
