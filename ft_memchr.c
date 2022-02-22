704aa65f85e7d702dd65d615a6eaea42e6c58c80
estarck <estarck@me.com>
2022-02-22 21:18:04 +0100

    memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,22 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: estarck <estarck@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/22 20:11:18 by estarck           #+#    #+#             */
-/*   Updated: 2022/02/22 21:15:17 by estarck          ###   ########.fr       */
+/*   Created: 2022/02/22 21:12:36 by estarck           #+#    #+#             */
+/*   Updated: 2022/02/22 21:17:53 by estarck          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*strchr(const char *s, int c)
+void *memchr(const void *s, int c, size_t n)
 {
-	while (*s != (char *)c)
-	{
-		if (*s == (char *)c)
-			return (s);
-		s++;
-	}
-	return (0x0);
+	
 }
