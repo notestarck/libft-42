56f9bf2c3c3f40cb87bdddb3582a3247995baed0
estarck <estarck@me.com>
2022-02-22 18:34:59 +0100

    strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: estarck <estarck@student.42mulhouse.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/22 18:23:36 by estarck           #+#    #+#             */
+/*   Updated: 2022/02/22 18:34:53 by estarck          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+size_t	ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize)
+{
+	unsigned int	i;
+	unsigned int	c;
+
+	i = 0;
+	c = 0;
+	while (src[c] != '\0')
+		c++;
+	if (dstsize != 0)
+	{
+		while (src[i] != '\0' && i < dstsize - 1)
+		{
+			dst[i] = src [i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (c);
+}
