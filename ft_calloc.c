f6bb510bbbfbf2967497f045958a9ecee2844a84
estarck <estarck@me.com>
2022-02-23 17:22:16 +0100

    calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,19 @@
 /*   By: estarck <estarck@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 16:44:09 by estarck           #+#    #+#             */
-/*   Updated: 2022/02/23 16:52:37 by estarck          ###   ########.fr       */
+/*   Updated: 2022/02/23 17:22:11 by estarck          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
+
+void	*calloc(size_t count, size_t size)
+{
+	void	*s;
+
+	s = malloc(count * size);
+	if (s == 0x0)
+		return (0x0);
+	ft_bzero(s, count * size);
+	return (s);
+}
