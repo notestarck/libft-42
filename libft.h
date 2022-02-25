7d7ddea468170f5aadc9d763afaa2ce1955b2164
estarck <estarck@me.com>
2022-02-25 12:03:04 +0100

    putnbr_fd

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: estarck <estarck@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 20:44:26 by estarck           #+#    #+#             */
-/*   Updated: 2022/02/25 11:39:34 by estarck          ###   ########.fr       */
+/*   Updated: 2022/02/25 11:46:00 by estarck          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ int		ft_toupper(int c);
 void	ft_bzero(void *s, size_t n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
